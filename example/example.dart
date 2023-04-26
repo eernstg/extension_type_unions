3e6be40fa54f744cd88372ac1ae71cc141216ad9
Erik Ernst <eernst@google.com>
2023-04-26 17:44:45 +0200

    Improved example

--- example/example.dart
+++ example/example.dart
@@ -1,13 +1,13 @@
 import 'package:inline_union_type/inline_union_type.dart';
 
-// Use `split`: Must receive a callback for every case, in order.
+// Use `split` to discriminate: Receive a callback for every case, in order.
 
 int doSplit(Union2<int, String> u) => u.split(
   (i) => i,
   (s) => s.length,
 );
 
-// Use `splitNamed`: Can handle subset of cases, otherwise calls `onOther`.
+// Use `splitNamed`: Can handle subset of cases, has `onOther`, may return null.
 
 int? doSplitNamed(Union2<int, String> u) => u.splitNamed(
   on1: (i) => i,
@@ -29,25 +29,30 @@ void main() {
   // to a union type `UnionN` with `N` type arguments. For example,
   // `Union2<int, String>.in1` turns an `int` into a `Union2<int, String>`.
   print(doSplit(Union2.in1(10))); // Prints '10'.
-  print(doSplit(Union2.in2('10'))); // '2'.
+  print(doSplit(Union2.in2('ab'))); // '2'.
 
   // We can also use the extension getters `asUnionNK` where `N` is the arity
   // of the union (the number of operands) and `K` is the position of the type
-  // argument describing the actual value. So, `asUnion21` for an `int` which
-  // gets the type `Union2<int, Never>` (which will work as a `Union2<int, S>`
+  // argument describing the actual value. So `asUnion21` on an `int` returns a
+  // result of type `Union2<int, Never>` (which will work as a `Union2<int, S>`
   // for any `S`).
   print(doSplit(10.asUnion21)); // '10'.
-  print(doSplit('10'.asUnion22)); // '2'.
+  print(doSplit('ab'.asUnion22)); // '2'.
   print(doSplitNamed(10.asUnion21)); // '10'.
-  print(doSplitNamed('10'.asUnion22)); // '2'.
+  print(doSplitNamed('ab'.asUnion22)); // '2'.
   print(doSplitNamedOther(10.asUnion21)); // '42'.
 
-  // We can't prevent the introduction of invalid union values, because it is
-  // always possible to force the type by an explicit cast. This case can be
-  // handled in a `splitNamed` invocation as shown in `doSplitNamedInvalid`.
-  var u = true as Union2<int, String>;
-  print(doSplitNamedInvalid(u)); // '-1'.
+  // It is a compile-time error to create a union typed value with wrong types.
+  // Union2<int, String> u1 = Union2.in1(true); // Error.
+  // Union2<int, String> u2 = Union2.in2(true); // Error.
+  // Union2<int, String> u3 = true.asUnion21; // Error.
+  // Union2<int, String> u4 = true.asUnion22; // Error.
 
-  // We can detect these cases, if we suspect that they will ever arise.
+  // However, we can't prevent the introduction of invalid union values,
+  // because it is always possible to force the type by an explicit cast. This
+  // situation can be handled in a `splitNamed` invocation as shown in
+  // `doSplitNamedInvalid`, and it can be detected using `isValid`.
+  var u = true as Union2<int, String>; // Bad, but no compile-time error.
+  print(doSplitNamedInvalid(u)); // '-1'.
   print(u.isValid); // 'false'.
 }
