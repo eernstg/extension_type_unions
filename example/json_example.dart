8b5583c2ecd9f9f761c3e3d8a8caf059d541d140
Erik Ernst <eernst@google.com>
2024-02-27 20:24:42 +0100

    Remove unused import

--- example/json_example.dart
+++ example/json_example.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:convert';
 import 'package:extension_type_unions/extension_type_json.dart';
 
 // Standard example from documentation of `jsonDecode`.
